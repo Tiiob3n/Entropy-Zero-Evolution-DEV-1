4829847718eeb19178570ac5e3641f8e5df6ebbf
Tiiob3n <150487534+Tiiob3n@users.noreply.github.com>
2026-04-22 14:23:18 +0200

    Unify gear outputs and hide alltheores gears
    Add JEI client script to hide alltheores gear items (JEIEvents.hideItems(/alltheores:.*_gear/)). Update server recipe unification to prefer Thermal gears by replacing #forge:gears/<material> outputs with thermal:<material>_gear for a list of common materials (iron, gold, copper, netherite, tin, lead, silver, nickel, bronze, electrum, invar, constantan, signalum, lumium, enderium, lapis, diamond, emerald, quartz, ruby, sapphire). Keeps existing bronze plate/dust unification.

--- kubejs/server_scripts/unfication.js
+++ kubejs/server_scripts/unfication.js
@@ -3,4 +3,17 @@ ServerEvents.recipes(event => {
     event.replaceOutput({}, '#forge:plates/bronze', 'alltheores:bronze_plate')
     event.replaceOutput({}, '#forge:dusts/bronze', 'alltheores:bronze_dust')
 
+    const gearMaterials = [
+        'iron', 'gold', 'copper', 'netherite', 'tin', 'lead', 'silver', 
+        'nickel', 'bronze', 'electrum', 'invar', 'constantan', 'signalum', 
+        'lumium', 'enderium', 'lapis', 'diamond', 'emerald', 'quartz', 
+        'ruby', 'sapphire'
+    ]
+    gearMaterials.forEach(mat => {
+        let gearTag = `#forge:gears/${mat}`
+        let preferredGear = `thermal:${mat}_gear`
+        
+        event.replaceOutput({}, gearTag, preferredGear)
+    })
+
 })
